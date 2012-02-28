9c77f08ed5e04e762acff27520b06f0ccd4dfb50
emijrp <emijrp@gmail.com>
2012-02-28 11:31:15 +0000

    md5 compatibility
    git-svn-id: https://wikiteam.googlecode.com/svn/trunk@341 31edc4fc-5e31-b4c4-d58b-c8bc928bcb95

--- commonsdownloader.py
+++ commonsdownloader.py
@@ -17,7 +17,10 @@
 
 import csv
 import datetime
-import md5
+try:
+    from hashlib import md5
+except ImportError:             # Python 2.4 compatibility
+    from md5 import new as md5
 import os
 import re
 import sys
