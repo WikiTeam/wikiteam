8e03c6ccf38b6968228dd67756cdc382a4849458
emijrp <emijrp@gmail.com>
2012-02-28 15:16:03 +0000

    years
    git-svn-id: https://wikiteam.googlecode.com/svn/trunk@344 31edc4fc-5e31-b4c4-d58b-c8bc928bcb95

--- dumpgenerator.py
+++ dumpgenerator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011 WikiTeam
+# Copyright (C) 2011-2012 WikiTeam
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
