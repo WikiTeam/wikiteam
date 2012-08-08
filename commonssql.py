f80bd3e0b6aa197c0aa1b12647041d02457c8e30
emijrp <emijrp@gmail.com>
2012-08-08 08:46:42 +0000

    git-svn-id: https://wikiteam.googlecode.com/svn/trunk@772 31edc4fc-5e31-b4c4-d58b-c8bc928bcb95

--- commonssql.py
+++ commonssql.py
@@ -26,9 +26,10 @@ f.close()
 
 #http://www.mediawiki.org/wiki/Manual:Image_table
 #http://www.mediawiki.org/wiki/Manual:Oldimage_table
+year = 2008
 queries = [
-    "SELECT /* commonssql.py SLOW_OK */ img_name, img_timestamp, img_user, img_user_text, img_size, img_width, img_height FROM image WHERE img_timestamp>=20070101000000 AND img_timestamp<20080101000000 ORDER BY img_timestamp ASC",
-    "SELECT /* commonssql.py SLOW_OK */ oi_archive_name AS img_name, oi_timestamp AS img_timestamp, oi_user AS img_user, oi_user_text AS img_user_text, oi_size AS img_size, oi_width AS img_width, oi_height AS img_height FROM oldimage WHERE oi_deleted=0 AND oi_timestamp>=20070101000000 AND oi_timestamp<20080101000000 ORDER BY oi_timestamp ASC" #do not get unavailable images
+    "SELECT /* commonssql.py SLOW_OK */ img_name, img_timestamp, img_user, img_user_text, img_size, img_width, img_height FROM image WHERE img_timestamp>=%d0101000000 AND img_timestamp<=%d1231235959 ORDER BY img_timestamp ASC" % (year, year),
+    "SELECT /* commonssql.py SLOW_OK */ oi_archive_name AS img_name, oi_timestamp AS img_timestamp, oi_user AS img_user, oi_user_text AS img_user_text, oi_size AS img_size, oi_width AS img_width, oi_height AS img_height FROM oldimage WHERE oi_deleted=0 AND oi_timestamp>=%d0101000000 AND oi_timestamp<=%d1231235959 ORDER BY oi_timestamp ASC" % (year, year), #do not get unavailable images
 ]
 
 f = csv.writer(open(filename, 'a'), delimiter='|', quotechar='"', quoting=csv.QUOTE_MINIMAL)
