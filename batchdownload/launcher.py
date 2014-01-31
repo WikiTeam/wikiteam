54f9798be0b3c54edb78bf3a90025a86bf79ebba
nemobis <federicoleva@tiscali.it>
2014-01-31 14:19:15 +0000

    Mark staging 7z files as .tmp to avoid uploading them by mistake
    git-svn-id: https://wikiteam.googlecode.com/svn/trunk@938 31edc4fc-5e31-b4c4-d58b-c8bc928bcb95

--- batchdownload/launcher.py
+++ batchdownload/launcher.py
@@ -110,8 +110,9 @@ for wiki in wikis:
         # Make a non-solid archive with all the text and metadata at default compression. You can also add config.txt if you don't care about your computer and user names being published or you don't use full paths so that they're not stored in it.
         subprocess.call('7z' + ' a -ms=off ../%s-history.xml.7z %s-history.xml %s-titles.txt index.html Special:Version.html errors.log' % (prefix, prefix, prefix), shell=True)
         # Now we add the images, if there are some, to create another archive, without recompressing everything, at the min compression rate, higher doesn't compress images much more.
-        subprocess.call('cp' + ' ../%s-history.xml.7z ../%s-wikidump.7z' % (prefix, prefix), shell=True)
-        subprocess.call('7z' + ' a -ms=off -mx=1 ../%s-wikidump.7z %s-images.txt images/' % (prefix, prefix), shell=True)
+        subprocess.call('cp' + ' ../%s-history.xml.7z ../%s-wikidump.7z.tmp' % (prefix, prefix), shell=True)
+        subprocess.call('7z' + ' a -ms=off -mx=1 ../%s-wikidump.7z.tmp %s-images.txt images/' % (prefix, prefix), shell=True)
+        subprocess.call('mv' + ' ../%s-wikidump.7z.tmp ../%s-wikidump.7z' % (prefix, prefix), shell=True)
         os.chdir('..')
         print 'Changed directory to', os.getcwd()
         time.sleep(1)
