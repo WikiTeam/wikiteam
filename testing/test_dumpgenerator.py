d04c0e5bceaaf151041e863d5eb1daf184381c9c
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-06-30 22:55:29 +0200

    first test

---
+++ testing/test_dumpgenerator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011-2014 WikiTeam developers
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import shutil
+import unittest
+from dumpgenerator import getImageFilenamesURLAPI
+
+class TestDumpgenerator(unittest.TestCase):
+    #Documentation
+    #http://revista.python.org.ar/1/html/unittest.html
+    #https://docs.python.org/2/library/unittest.html
+    
+    def test_getImageFilenamesURLAPI(self):
+        #Checks if this filename かずさアノテーション_-_ソーシャル・ゲノム・アノテーション.jpg is well parsed from API
+        #http://wiki.annotation.jp/images/0/02/%E3%81%8B%E3%81%9A%E3%81%95%E3%82%A2%E3%83%8E%E3%83%86%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3_-_%E3%82%BD%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%BB%E3%82%B2%E3%83%8E%E3%83%A0%E3%83%BB%E3%82%A2%E3%83%8E%E3%83%86%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3.jpg
+        config = {
+            'api': 'http://wiki.annotation.jp/api.php', 
+            'delay': 0, 
+        }
+        result = getImageFilenamesURLAPI(config=config)
+        self.assertTrue(u'かずさアノテーション - ソーシャル・ゲノム・アノテーション.jpg' in [filename for filename, url, uploader in result])
+
+if __name__ == '__main__':
+    #copying dumpgenerator.py to this directory
+    shutil.copy2('../dumpgenerator.py', './dumpgenerator.py')
+    
+    unittest.main()
