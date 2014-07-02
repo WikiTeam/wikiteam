636c6a91dff894a2dc0e5f1e3fbd3c1b3957b547
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-07-02 16:19:49 +0200

    adding spider for neoseeker, updating list, adding info file

---
+++ listsofwikis/neoseeker-spider.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 WikiTeam developers
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import requests
+
+def main():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0',
+    }
+    
+    url = 'http://neowiki.neoseeker.com/wiki/Special:WikiList'
+    r = requests.get(url, headers=headers)
+    raw = r.text
+    m = re.findall(ur'<li><a href=\'([^>]+?)/wiki/\'>', raw)
+    for i in m:
+        print i + '/w/api.php'
+    
+if __name__ == '__main__':
+    main()
