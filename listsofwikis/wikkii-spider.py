514d5fea0e7d90b8461531454b30ee292efd3ebe
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-07-02 15:57:15 +0200

    removing unused modules

--- listsofwikis/wikkii-spider.py
+++ listsofwikis/wikkii-spider.py
@@ -15,11 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import json
-import random
 import re
 import requests
-import time
 
 def main():
     headers = {
