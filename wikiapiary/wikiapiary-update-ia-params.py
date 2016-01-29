6dabef598049b7d823dc69b2d1d51187fdef2ab8
emijrp <emijrp@gmail.com>
2016-01-29 15:00:33 +0100

    wikiapiary bots

---
+++ wikiapiary/wikiapiary-update-ia-params.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 WikiTeam
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import urllib
+
+import pywikibot
+from pywikibot import pagegenerators
+
+def main():
+    site = pywikibot.Site('wikiapiary', 'wikiapiary')
+    catname = 'Category:Website'
+    cat = pywikibot.Category(site, catname)
+    gen = pagegenerators.CategorizedPageGenerator(cat)
+    pre = pagegenerators.PreloadingGenerator(gen)
+    
+    for page in pre:
+        if page.isRedirectPage():
+            continue
+        
+        wtitle = page.title()
+        wtext = page.text
+                
+        if re.search('Internet Archive', wtext):
+            #print('It has IA parameter')
+            pass
+        else:
+            print('\n','#'*50,'\n',wtitle,'\n','#'*50)
+            print('https://wikiapiary.com/wiki/%s' % (re.sub(' ', ',¡_', wtitle)))
+            print('Missing IA parameter')
+            
+            if re.search(r'(?i)API URL=http', wtext):
+                apiurl = re.findall(r'(?i)API URL=(http[^\n]+?)\n', wtext)[0]
+                print('API:', apiurl)
+            else:
+                print('No API found in WikiApiary, skiping')
+                continue
+            
+            urliasearch = 'https://archive.org/search.php?query=originalurl:"%s"' % (apiurl)
+            f = urllib.request.urlopen(urliasearch)
+            raw = f.read().decode('utf-8')
+            if re.search(r'(?i)Your search did not match any items', raw):
+                print('No dumps found at Internet Archive')
+            else:
+                itemidentifier = re.findall(r'<a href="/details/([^ ]+?)" title=', raw)[0]
+                itemurl = 'https://archive.org/details/%s' % (itemidentifier)
+                print('Item found:',itemurl)
+                
+                metaurl = 'https://archive.org/download/%s/%s_files.xml' % (itemidentifier, itemidentifier)
+                g = urllib.request.urlopen(metaurl)
+                raw2 = g.read().decode('utf-8')
+                itemfiles = re.findall(r'(?im)<file name="[^ ]+-(\d{8})-[^ ]+" source="original">\s*<mtime>\d+</mtime>\s*<size>(\d+)</size>', raw2)
+                itemfiles = [[int(x), int(y)] for x, y in itemfiles]
+                itemfiles.sort(reverse=True)
+                print(itemfiles)
+                itemdate = str(itemfiles[0][0])[0:4] + '/' + str(itemfiles[0][0])[4:6] + '/' + str(itemfiles[0][0])[6:8]
+                itemsize = itemfiles[0][1]
+                
+                iaparams = """|Internet Archive identifier=%s
+|Internet Archive URL=%s
+|Internet Archive added date=%s 00:00:00 
+|Internet Archive file size=%s""" % (itemidentifier, itemurl, itemdate, itemsize)
+                newtext = page.text
+                newtext = re.sub(r'(?im)\}\}\n', '%s\n}}\n' % (iaparams), newtext)
+                pywikibot.showDiff(page.text, newtext)
+                page.text = newtext
+                page.save('BOT - Adding dump details: %s, %s, %s bytes' % (itemidentifier, itemdate, itemsize))
+    
+if __name__ == "__main__":
+    main()
+
