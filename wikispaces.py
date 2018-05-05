0574b5f33a2f49da1634692d5183e2e7503a86f2
emijrp <emijrp@gmail.com>
2018-05-05 10:11:21 +0200

    second version, it downloads all, including sitemap and mainpage

--- wikispaces.py
+++ wikispaces.py
@@ -25,12 +25,13 @@ import sys
 import time
 import urllib.request
 
-def saveURL(url='', filename='', path=''):
-    wikidomain = url.split('//')[1].split('/')[0]
+def saveURL(wikidomain='', url='', filename='', path=''):
+    filename2 = '%s/%s' % (wikidomain, filename)
     if path:
         filename2 = '%s/%s/%s' % (wikidomain, path, filename)
-    else:
-        filename2 = '%s/%s' % (wikidomain, filename)
+    #print(wikidomain)
+    #print(url)
+    #print(filename2)
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
@@ -49,37 +50,76 @@ def saveURL(url='', filename='', path=''):
             except:
                 sleep = sleep * 2
 
-def downloadPage(wikiurl='', pagename=''):
+def undoHTMLEntities(text=''):
+    """ Undo some HTML codes """
+
+    # i guess only < > & " ' need conversion
+    # http://www.w3schools.com/html/html_entities.asp
+    text = re.sub('&lt;', '<', text)
+    text = re.sub('&gt;', '>', text)
+    text = re.sub('&amp;', '&', text)
+    text = re.sub('&quot;', '"', text)
+    text = re.sub('&#039;', '\'', text)
+
+    return text
+
+def convertHTML2Wikitext(wikidomain='', filename='', path=''):
+    wikitext = ''
+    with open('%s/%s/%s' % (wikidomain, path, filename), 'r') as f:
+        wikitext = f.read()
+    with open('%s/%s/%s' % (wikidomain, path, filename), 'w') as f:
+        m = re.findall(r'(?im)<div class="WikispacesContent WikispacesBs3">\s*<pre>', wikitext)
+        if m:
+            try:
+                wikitext = wikitext.split(m[0])[1].split('</pre>')[0].strip()
+                wikitext = undoHTMLEntities(text=wikitext)
+            except:
+                wikitext = ''
+                print('Error extracting wikitext.')
+        else:
+            wikitext = ''
+            print('Error extracting wikitext.')
+        f.write(wikitext)
+
+def downloadPage(wikidomain='', wikiurl='', pagename=''):
     pagenameplus = re.sub(' ', '+', pagename)
     pagename_ = urllib.parse.quote(pagename)
-    #page current revision
-    pageurl = '%s/page/code/%s' % (wikiurl, pagename_)
-    filename = '%s.wikitext' % (pagenameplus)
-    saveURL(url=pageurl, filename=filename, path='pages')
+    
+    #page current revision (html & wikitext)
+    pageurl = '%s/%s' % (wikiurl, pagename_)
+    filename = '%s.html' % (pagenameplus)
+    saveURL(wikidomain=wikidomain, url=pageurl, filename=filename, path='pages')
+    pageurl2 = '%s/page/code/%s' % (wikiurl, pagename_)
+    filename2 = '%s.wikitext' % (pagenameplus)
+    saveURL(wikidomain=wikidomain, url=pageurl2, filename=filename2, path='pages')
+    convertHTML2Wikitext(wikidomain=wikidomain, filename=filename2, path='pages')
+    
     #csv with page history
     csvurl = '%s/page/history/%s?utable=WikiTablePageHistoryList&ut_csv=1' % (wikiurl, pagename_)
     csvfilename = '%s.history.csv' % (pagenameplus)
-    saveURL(url=csvurl, filename=csvfilename, path='pages')
+    saveURL(wikidomain=wikidomain, url=csvurl, filename=csvfilename, path='pages')
 
-def downloadFile(wikiurl='', filename=''):
+def downloadFile(wikidomain='', wikiurl='', filename=''):
     filenameplus = re.sub(' ', '+', filename)
     filename_ = urllib.parse.quote(filename)
+    
     #file full resolution
     fileurl = '%s/file/view/%s' % (wikiurl, filename_)
     filename = filenameplus
-    saveURL(url=fileurl, filename=filename, path='files')
+    saveURL(wikidomain=wikidomain, url=fileurl, filename=filename, path='files')
+    
     #csv with file history
     csvurl = '%s/file/detail/%s?utable=WikiTablePageList&ut_csv=1' % (wikiurl, filename_)
     csvfilename = '%s.history.csv' % (filenameplus)
-    saveURL(url=csvurl, filename=csvfilename, path='files')
+    saveURL(wikidomain=wikidomain, url=csvurl, filename=csvfilename, path='files')
 
-def downloadPagesAndFiles(wikiurl=''):
+def downloadPagesAndFiles(wikidomain='', wikiurl=''):
     print('Downloading Pages and Files from %s' % (wikiurl))
     #csv all pages and files
     csvurl = '%s/space/content?utable=WikiTablePageList&ut_csv=1' % (wikiurl)
-    saveURL(url=csvurl, filename='pages-and-files.csv')
+    saveURL(wikidomain=wikidomain, url=csvurl, filename='pages-and-files.csv', path='')
     #download every page and file
-    with open('pages-and-files.csv', 'r') as csvfile:
+    with open('%s/pages-and-files.csv' % (wikidomain), 'r') as csvfile:
         filesc = 0
         pagesc = 0
         rows = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -88,22 +128,27 @@ def downloadPagesAndFiles(wikiurl=''):
                 filesc += 1
                 filename = row[1]
                 print('Downloading file: %s' % (filename))
-                downloadFile(wikiurl=wikiurl, filename=filename)
+                downloadFile(wikidomain=wikidomain, wikiurl=wikiurl, filename=filename)
             elif row[0] == 'page':
                 pagesc += 1
                 pagename = row[1]
                 print('Downloading page: %s' % (pagename))
-                downloadPage(wikiurl=wikiurl, pagename=pagename)
+                downloadPage(wikidomain=wikidomain, wikiurl=wikiurl, pagename=pagename)
     print('Downloaded %d pages' % (pagesc))
     print('Downloaded %d files' % (filesc))
 
-def downloadMainPage(wikiurl=''):
-    saveURL(url=wikiurl, filename='index.html', path='')
+def downloadSitemap(wikidomain='', wikiurl=''):
+    saveURL(wikidomain=wikidomain, url=wikiurl, filename='sitemap.xml', path='')
+
+def downloadMainPage(wikidomain='', wikiurl=''):
+    saveURL(wikidomain=wikidomain, url=wikiurl, filename='index.html', path='')
 
 def main():
     if len(sys.argv) < 2:
+        print('Please, introduce a wikispaces wiki url.\nExample: https://yourwiki.wikispaces.com')
         sys.exit()
     wikiurl = sys.argv[1]
+    wikiurl = wikiurl.rstrip('/')
     if not wikiurl or not '//' in wikiurl:
         print('Please, introduce a wikispaces wiki url.\nExample: https://yourwiki.wikispaces.com')
         sys.exit()
@@ -113,8 +158,10 @@ def main():
         os.makedirs('%s/files' % (wikidomain))
     if not os.path.exists('%s/pages' % (wikidomain)):
         os.makedirs('%s/pages' % (wikidomain))
-    downloadPagesAndFiles(wikiurl=wikiurl)
-    downloadMainPage(wikiurl=wikiurl)
+    downloadPagesAndFiles(wikidomain=wikidomain, wikiurl=wikiurl)
+    sitemapurl = 'https://%s/sitemap.xml' % (wikidomain)
+    downloadSitemap(wikidomain=wikidomain, wikiurl=sitemapurl)
+    downloadMainPage(wikidomain=wikidomain, wikiurl=wikiurl)
 
 if __name__ == "__main__":
     main()
