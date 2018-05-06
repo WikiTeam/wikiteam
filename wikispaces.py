af680ced4a4fb5d44c20805278e99f0d094643d3
emijrp <emijrp@gmail.com>
2018-05-06 14:07:48 +0200

    help, params

--- wikispaces.py
+++ wikispaces.py
@@ -29,15 +29,16 @@ import urllib.request
 
 # Requirements:
 # zip command (apt-get install zip)
-# ia command (pip install internetarchive)
+# ia command (pip install internetarchive, and configured properly)
 
-def saveURL(wikidomain='', url='', filename='', path=''):
+def saveURL(wikidomain='', url='', filename='', path='', overwrite=False):
     filename2 = '%s/%s' % (wikidomain, filename)
     if path:
         filename2 = '%s/%s/%s' % (wikidomain, path, filename)
-    #print(wikidomain)
-    #print(url)
-    #print(filename2)
+    if os.path.exists(filename2):
+        if not overwrite:
+            print('Warning: file exists on disk. Skipping download. Force download with parameter --overwrite')
+            return
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
@@ -52,9 +53,10 @@ def saveURL(wikidomain='', url='', filename='', path=''):
                 print('Retry in %s seconds...' % (sleep))
                 time.sleep(sleep)
                 urllib.request.urlretrieve(url, filename2)
-                break
+                return
             except:
                 sleep = sleep * 2
+        print('Download failed')
 
 def undoHTMLEntities(text=''):
     """ Undo some HTML codes """
@@ -69,7 +71,9 @@ def undoHTMLEntities(text=''):
 
     return text
 
-def convertHTML2Wikitext(wikidomain='', filename='', path=''):
+def convertHTML2Wikitext(wikidomain='', filename='', path='', overwrite=False):
+    if not overwrite:
+        return
     wikitext = ''
     wikitextfile = '%s/%s/%s' % (wikidomain, path, filename)
     if not os.path.exists(wikitextfile):
@@ -91,71 +95,81 @@ def convertHTML2Wikitext(wikidomain='', filename='', path=''):
             print('Error extracting wikitext.')
         f.write(wikitext)
 
-def downloadPage(wikidomain='', wikiurl='', pagename=''):
+def downloadPage(wikidomain='', wikiurl='', pagename='', overwrite=False):
     pagenameplus = re.sub(' ', '+', pagename)
     pagename_ = urllib.parse.quote(pagename)
     
     #page current revision (html & wikitext)
     pageurl = '%s/%s' % (wikiurl, pagename_)
     filename = '%s.html' % (pagenameplus)
-    saveURL(wikidomain=wikidomain, url=pageurl, filename=filename, path='pages')
+    print('Downloading page: %s' % (filename))
+    saveURL(wikidomain=wikidomain, url=pageurl, filename=filename, path='pages', overwrite=overwrite)
     pageurl2 = '%s/page/code/%s' % (wikiurl, pagename_)
     filename2 = '%s.wikitext' % (pagenameplus)
-    saveURL(wikidomain=wikidomain, url=pageurl2, filename=filename2, path='pages')
-    convertHTML2Wikitext(wikidomain=wikidomain, filename=filename2, path='pages')
+    print('Downloading page: %s' % (filename2))
+    saveURL(wikidomain=wikidomain, url=pageurl2, filename=filename2, path='pages', overwrite=overwrite)
+    convertHTML2Wikitext(wikidomain=wikidomain, filename=filename2, path='pages', overwrite=overwrite)
     
     #csv with page history
     csvurl = '%s/page/history/%s?utable=WikiTablePageHistoryList&ut_csv=1' % (wikiurl, pagename_)
     csvfilename = '%s.history.csv' % (pagenameplus)
-    saveURL(wikidomain=wikidomain, url=csvurl, filename=csvfilename, path='pages')
+    print('Downloading page: %s' % (csvfilename))
+    saveURL(wikidomain=wikidomain, url=csvurl, filename=csvfilename, path='pages', overwrite=overwrite)
 
-def downloadFile(wikidomain='', wikiurl='', filename=''):
+def downloadFile(wikidomain='', wikiurl='', filename='', overwrite=False):
     filenameplus = re.sub(' ', '+', filename)
     filename_ = urllib.parse.quote(filename)
     
     #file full resolution
     fileurl = '%s/file/view/%s' % (wikiurl, filename_)
     filename = filenameplus
-    saveURL(wikidomain=wikidomain, url=fileurl, filename=filename, path='files')
+    print('Downloading file: %s' % (filename))
+    saveURL(wikidomain=wikidomain, url=fileurl, filename=filename, path='files', overwrite=overwrite)
     
     #csv with file history
     csvurl = '%s/file/detail/%s?utable=WikiTablePageList&ut_csv=1' % (wikiurl, filename_)
     csvfilename = '%s.history.csv' % (filenameplus)
-    saveURL(wikidomain=wikidomain, url=csvurl, filename=csvfilename, path='files')
+    print('Downloading file: %s' % (csvfilename))
+    saveURL(wikidomain=wikidomain, url=csvurl, filename=csvfilename, path='files', overwrite=overwrite)
 
-def downloadPagesAndFiles(wikidomain='', wikiurl=''):
+def downloadPagesAndFiles(wikidomain='', wikiurl='', overwrite=False):
     print('Downloading Pages and Files from %s' % (wikiurl))
     #csv all pages and files
     csvurl = '%s/space/content?utable=WikiTablePageList&ut_csv=1' % (wikiurl)
     saveURL(wikidomain=wikidomain, url=csvurl, filename='pages-and-files.csv', path='')
     #download every page and file
+    totallines = 0
+    with open('%s/pages-and-files.csv' % (wikidomain), 'r') as f:
+        totallines = len(f.read().splitlines()) - 1
     with open('%s/pages-and-files.csv' % (wikidomain), 'r') as csvfile:
         filesc = 0
         pagesc = 0
+        print('This wiki has %d pages and files' % (totallines))
         rows = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in rows:
             if row[0] == 'file':
                 filesc += 1
                 filename = row[1]
-                print('Downloading file: %s' % (filename))
-                downloadFile(wikidomain=wikidomain, wikiurl=wikiurl, filename=filename)
+                downloadFile(wikidomain=wikidomain, wikiurl=wikiurl, filename=filename, overwrite=overwrite)
             elif row[0] == 'page':
                 pagesc += 1
                 pagename = row[1]
-                print('Downloading page: %s' % (pagename))
-                downloadPage(wikidomain=wikidomain, wikiurl=wikiurl, pagename=pagename)
+                downloadPage(wikidomain=wikidomain, wikiurl=wikiurl, pagename=pagename, overwrite=overwrite)
+            if (filesc + pagesc) % 10 == 0:
+                print('  Progress: %d of %d' % ((filesc + pagesc), totallines))
+        print('  Progress: %d of %d' % ((filesc + pagesc), totallines))
     print('Downloaded %d pages' % (pagesc))
     print('Downloaded %d files' % (filesc))
 
-def downloadSitemap(wikidomain='', wikiurl=''):
+def downloadSitemap(wikidomain='', wikiurl='', overwrite=False):
     print('Downloading sitemap.xml')
-    saveURL(wikidomain=wikidomain, url=wikiurl, filename='sitemap.xml', path='')
+    saveURL(wikidomain=wikidomain, url=wikiurl, filename='sitemap.xml', path='', overwrite=overwrite)
 
-def downloadMainPage(wikidomain='', wikiurl=''):
+def downloadMainPage(wikidomain='', wikiurl='', overwrite=False):
     print('Downloading index.html')
-    saveURL(wikidomain=wikidomain, url=wikiurl, filename='index.html', path='')
+    saveURL(wikidomain=wikidomain, url=wikiurl, filename='index.html', path='', overwrite=overwrite)
 
-def downloadLogo(wikidomain='', wikiurl=''):
+def downloadLogo(wikidomain='', wikiurl='', overwrite=False):
     index = '%s/index.html' % (wikidomain)
     if os.path.exists(index):
         with open(index, 'r') as f:
@@ -164,25 +178,56 @@ def downloadLogo(wikidomain='', wikiurl=''):
                 logourl = m[0]
                 logofilename = logourl.split('/')[-1]
                 print('Downloading logo')
-                saveURL(wikidomain=wikidomain, url=logourl, filename=logofilename, path='')
+                saveURL(wikidomain=wikidomain, url=logourl, filename=logofilename, path='', overwrite=overwrite)
                 return logofilename
     return ''
+
+def printhelp():
+    helptext = """This script downloads (and uploads) WikiSpaces wikis.
+
+Parameters available:
+
+--upload: upload compressed file with downloaded wiki
+--admin: add item to WikiTeam collection (if you are an admin in that collection)
+--overwrite: download again even if files exists locally
+--overwrite-ia: upload again to Internet Archive even if item exists there
+--help: prints this help text
+
+Examples:
+
+python3 wikispaces.py https://mywiki.wikispaces.com
+   It downloads that wiki
+
+python3 wikispaces.py wikis.txt
+   It downloads a list of wikis (file format is a URL per line)
+
+python3 wikispaces.py https://mywiki.wikispaces.com --upload
+   It downloads that wiki, compress it and uploading to Internet Archive
+"""
+    print(helptext)
+    sys.exit()
 
 def main():
     upload = False
     isadmin = False
+    overwrite = False
+    overwriteia = False
     if len(sys.argv) < 2:
-        print('Please, introduce a wikispaces wiki url or filename.\nExample: https://yourwiki.wikispaces.com or mylistofwikis.txt')
-        sys.exit()
+        printhelp()
     param = sys.argv[1]
     if not param:
-        print('Please, introduce a wikispaces wiki url or filename.\nExample: https://yourwiki.wikispaces.com or mylistofwikis.txt')
-        sys.exit()
+        printhelp()
     if len(sys.argv) > 2:
         if '--upload' in sys.argv:
             upload = True
         if '--admin' in sys.argv:
             isadmin = True
+        if '--overwrite' in sys.argv:
+            overwrite = True
+        if '--overwrite-ia' in sys.argv:
+            overwriteia = True
+        if '--help' in sys.argv:
+            printhelp()
     
     wikilist = []
     if '://' in param:
@@ -207,13 +252,20 @@ def main():
         if not os.path.exists(dirpages):
             print('Creating directory %s' % (dirpages))
             os.makedirs(dirpages)
-        downloadPagesAndFiles(wikidomain=wikidomain, wikiurl=wikiurl)
+        downloadPagesAndFiles(wikidomain=wikidomain, wikiurl=wikiurl, overwrite=overwrite)
         sitemapurl = 'https://%s/sitemap.xml' % (wikidomain)
-        downloadSitemap(wikidomain=wikidomain, wikiurl=sitemapurl)
-        downloadMainPage(wikidomain=wikidomain, wikiurl=wikiurl)
-        logofilename = downloadLogo(wikidomain=wikidomain, wikiurl=wikiurl)
+        downloadSitemap(wikidomain=wikidomain, wikiurl=sitemapurl, overwrite=overwrite)
+        downloadMainPage(wikidomain=wikidomain, wikiurl=wikiurl, overwrite=overwrite)
+        logofilename = downloadLogo(wikidomain=wikidomain, wikiurl=wikiurl, overwrite=overwrite)
         
         if upload:
+            itemid = 'wiki-%s' % (wikidomain)
+            iahtml = urllib.request.urlopen('https://archive.org/details/%s' % (itemid)).read().decode('utf-8')
+            if not re.findall(r'Item cannot be found', iahtml):
+                if not overwriteia:
+                    print('Warning: item exists on Internet Archive. Skipping upload. Force upload with parameter --overwrite-ia')
+                    continue
+                
             print('\nCompressing dump...')
             wikidir = wikidomain
             os.chdir(wikidir)
@@ -231,7 +283,6 @@ def main():
             f = open(indexfilename, 'r')
             indexhtml = f.read()
             f.close()
-            itemid = 'wiki-%s' % (wikidomain)
             wikititle = ''
             try:
                 wikititle = indexhtml.split('wiki: {')[1].split('}')[0].split("text: '")[1].split("',")[0].strip()
