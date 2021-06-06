e55de36cb7b323127fede1d89826a577d525b961
Shreyas Minocha <sm-git@riseup.net>
2021-06-07 02:40:08 +0530

    Fix image resuming

--- dumpgenerator.py
+++ dumpgenerator.py
@@ -2242,7 +2242,7 @@ def resumePreviousDump(config={}, other={}):
         # checking images directory
         listdir = []
         try:
-            listdir = [n.decode('utf-8') for n in os.listdir('%s/images' % (config['path']))]
+            listdir = os.listdir('%s/images' % (config['path']))
         except:
             pass  # probably directory does not exist
         listdir.sort()
