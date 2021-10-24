ba4d31f83dbae1dacf83def15a89a8fe5488c0b9
Victor Gambier <banon.victor@gmail.com>
2021-10-24 15:12:41 +0200

    Fix md5 file being fed unicode string

--- wikiteam3/dumpgenerator/generator.py
+++ wikiteam3/dumpgenerator/generator.py
@@ -120,7 +120,7 @@ class DumpGenerator:
         """Truncate filenames when downloading images with large filenames"""
         return (
             filename[: other["filenamelimit"]]
-            + md5(str(filename)).hexdigest()
+            + md5(str(filename).encode('utf-8')).hexdigest()
             + "."
             + filename.split(".")[-1]
         )
