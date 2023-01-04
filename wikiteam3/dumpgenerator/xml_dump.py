6678156ff7d98245ee667450cf1d2353716db378
yzqzss <yzqzss@yandex.com>
2023-01-05 05:32:06 +0800

    print "X title, y edits" instead of "X more revisions exported"
    for --xmlrevisions

--- wikiteam3/dumpgenerator/xml_dump.py
+++ wikiteam3/dumpgenerator/xml_dump.py
@@ -7,7 +7,7 @@ from .exceptions import PageMissingError
 from .log_error import logerror
 from .page_titles import readTitles
 from .page_xml import getXMLPage
-from .util import cleanXML
+from .util import cleanXML, undoHTMLEntities
 from .xml_header import getXMLHeader
 from .xml_revisions import getXMLRevisions
 from .xml_truncate import truncateXMLDump
@@ -29,6 +29,11 @@ def generateXMLDump(config={}, titles=[], start=None, session=None):
 
     if config["xmlrevisions"]:
         if start:
+            print(
+                "Removing the last chunk of past XML dump: it is probably incomplete."
+            )
+            truncateXMLDump("{}/{}".format(config["path"], xmlfilename))
+
             print(f"WARNING: will try to start the download from title: {start}")
             xmlfile = open(
                 "{}/{}".format(config["path"], xmlfilename), "a", encoding="utf-8"
@@ -38,16 +43,18 @@ def generateXMLDump(config={}, titles=[], start=None, session=None):
             xmlfile = open(
                 "{}/{}".format(config["path"], xmlfilename), "w", encoding="utf-8"
             )
-            xmlfile.write(str(header))
+            xmlfile.write(header)
         try:
             r_timestamp = "<timestamp>([^<]+)</timestamp>"
             for xml in getXMLRevisions(config=config, session=session, start=start):
                 numrevs = len(re.findall(r_timestamp, xml))
                 # Due to how generators work, it's expected this may be less
-                # TODO: get the page title and reuse the usual format "X title, y edits"
-                print("        %d more revisions exported" % numrevs)
                 xml = cleanXML(xml=xml)
-                xmlfile.write(str(xml))
+                xmlfile.write(xml)
+
+                xmltitle = re.search(r"<title>([^<]+)</title>", xml)
+                title = undoHTMLEntities(text=xmltitle.group(1))
+                print(f'{title}, {numrevs} edits (--xmlrevisions))')
                 Delay(config=config, session=session)
         except AttributeError as e:
             print(e)
@@ -72,7 +79,7 @@ def generateXMLDump(config={}, titles=[], start=None, session=None):
             xmlfile = open(
                 "{}/{}".format(config["path"], xmlfilename), "w", encoding="utf-8"
             )
-            xmlfile.write(str(header))
+            xmlfile.write(header)
             xmlfile.close()
 
         xmlfile = open(
@@ -92,7 +99,7 @@ def generateXMLDump(config={}, titles=[], start=None, session=None):
             try:
                 for xml in getXMLPage(config=config, title=title, session=session):
                     xml = cleanXML(xml=xml)
-                    xmlfile.write(str(xml))
+                    xmlfile.write(xml)
             except PageMissingError:
                 logerror(
                     config=config,
@@ -105,6 +112,6 @@ def generateXMLDump(config={}, titles=[], start=None, session=None):
             # (logged in errors log)
             c += 1
 
-    xmlfile.write(str(footer))
+    xmlfile.write(footer)
     xmlfile.close()
     print("XML dump saved at...", xmlfilename)
