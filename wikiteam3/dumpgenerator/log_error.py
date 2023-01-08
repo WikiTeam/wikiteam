63270647781933df213662029ca1a5e7ab37ee9a
yzqzss <yzqzss@yandex.com>
2023-01-09 03:05:16 +0800

    Add to_stdout parameter to logerror() for printing errors to stdout

--- wikiteam3/dumpgenerator/log_error.py
+++ wikiteam3/dumpgenerator/log_error.py
@@ -1,12 +1,14 @@
 import datetime
 
 
-def logerror(config={}, text=""):
-    """Log error in file"""
+def logerror(config={},to_stdout=False , text="") -> None:
+    """Log error in errors.log"""
     if text:
         with open("%s/errors.log" % (config["path"]), "a", encoding="utf-8") as outfile:
             output = "{}: {}\n".format(
                 datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 text,
             )
-            outfile.write(str(output))
+            outfile.write(output)
+    if to_stdout:
+        print(text)
