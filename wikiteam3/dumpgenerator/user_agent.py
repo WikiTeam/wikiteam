aed4cdd78e2e83f3ccf884ba4345857c9ae6b98d
yzqzss <yzqzss@yandex.com>
2023-01-10 20:12:47 +0800

    update UA

--- wikiteam3/dumpgenerator/user_agent.py
+++ wikiteam3/dumpgenerator/user_agent.py
@@ -4,6 +4,7 @@ def getUserAgent():
         # firefox
         # 'Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
         # 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0',
-        "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
+        # "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
     ]
     return useragents[0]
