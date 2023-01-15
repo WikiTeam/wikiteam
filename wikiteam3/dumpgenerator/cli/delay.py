a25be5e1c0a63b65fd99f0673df729cc20dbbd5d
Janet Cobb <jason.e.cobb@gmail.com>
2023-01-15 17:15:31 -0500

    Properly synchronize Delay (#94)

--- wikiteam3/dumpgenerator/cli/delay.py
+++ wikiteam3/dumpgenerator/cli/delay.py
@@ -6,26 +6,30 @@ import sys
 from wikiteam3.dumpgenerator.config import Config
 
 class Delay:
-
-    done: bool = True
+    done: bool = False
+    lock: threading.Lock = threading.Lock()
     ellipses: str = "."
 
     def animate(self):
-        while not self.done:
-            print("\r" + self.ellipses, end="")
-            self.ellipses += "."
+        while True:
+            with self.lock:
+                if self.done:
+                    return
+
+                print("\r" + self.ellipses, end="")
+                self.ellipses += "."
+
             time.sleep(0.1)
 
     def __init__(self, config: Config=None, session=None):
         """Add a delay if configured for that"""
         if config.delay > 0:
-            self.done = False
-
             ellipses_animation = threading.Thread(target=self.animate)
             ellipses_animation.daemon = True
             ellipses_animation.start()
 
             time.sleep(config.delay)
-            self.done = True
 
-            print("\r    \r", end="")
+            with self.lock:
+                self.done = True
+                print("\r" + " " * len(self.ellipses) + "\r", end="")
